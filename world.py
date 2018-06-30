ad4128c54f3fc8829e621b79748877f6a79d191b
Keaton Drebes <keatondrebes@Keatons-MacBook-Air.local>
2018-06-30 05:57:35 -0700

    changed redundant import of objects

--- world.py
+++ world.py
@@ -1,13 +1,12 @@
 #simulation
-import objects
 import player 
 import checkpoints
 
 #
 #setup
-center = objects.gravobj(200,200,300)
-player = objects.gravobj(500,500,400)
-third = objects.gravobj(200,500,800)
+center = player.objects.gravobj(200,200,300)
+player = player.objects.gravobj(500,500,400)
+third = player.objects.gravobj(200,500,800)
 gravList = [center,player,third]
 
 def getGravList():
